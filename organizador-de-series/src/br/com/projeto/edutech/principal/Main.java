d86a4423ccdcea522fb9be5ceb15aefd373f976f
João Gabriel Nascimento da Silva <jgabriel10x@gmail.com>
2021-08-18 15:01:01 -0300

    main movido e comentarios do netbeans traduzidos
    o método main da classe InterfacePrincipal foi movido para a classe Main
    do pacote br.com.projeto.edutech.principal

--- organizador-de-series/src/br/com/projeto/edutech/principal/Main.java
+++ organizador-de-series/src/br/com/projeto/edutech/principal/Main.java
@@ -1,9 +1,36 @@
 package br.com.projeto.edutech.principal;
+import br.com.projeto.edutech.interface_grafica.InterfacePrincipal;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// Classe principal do programa
+		
+		/* Define o visual Nimbus */
+        /* Se o Nimbus (introduzido no Java SE 6) não estiver disponivel, permanece com o visual padrão.
+         * Para detalhes veja: http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(InterfacePrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(InterfacePrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(InterfacePrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(InterfacePrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+		
+		java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new InterfacePrincipal().setVisible(true);
+            }
+        });
 
 	}
 
